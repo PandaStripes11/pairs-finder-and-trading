d2274dbdf52d61815cee310aa0ea76d4d1986e19
Donte Truong <dontetruong@gmail.com>
2025-07-30 01:49:26 -0500

    Loading spinner addition

---
+++ frontend/src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
